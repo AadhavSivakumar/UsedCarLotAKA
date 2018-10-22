d63aa2310a8ca929866184c24e93a161b1d0d44f
mineramc <chenandmiller@gmail.com>
2018-10-22 09:10:20 -0700

    Update Car.java

--- Car.java
+++ Car.java
@@ -2,17 +2,21 @@ public abstract class Car{
 
 private int price;
 private String model;
+private int vin;
 
 public Car(){
   price=0;
   model="undef";
+  vin=0;
 }
 
 public Car (int p, String mod){
   price=p;
   model=mod;
 }
-
+public void setVin(int Vin){
+  vin = Vin;
+}
 public void setPrice(int p){
   price=p;
 }
@@ -20,8 +24,9 @@ public void setPrice(int p){
 public int getPrice(){
   return  price;
 }
-
-
+public int getVin(){
+  return vin;
+}
 public void setModel(String m){
   model=m;
 }
